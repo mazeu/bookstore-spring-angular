62c3aa4b5bd99bf282fa10082050893fee73e207
mazeu <marciomazeu@hotmail.com>
2021-07-05 20:38:01 -0300

    Repository created

---
+++ src/main/java/com/mazeu/bookstore/repositories/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package com.mazeu.bookstore.repositories;
+
+import com.mazeu.bookstore.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
---
+++ src/main/java/com/mazeu/bookstore/repositories/LivroRepository.java
@@ -0,0 +1,10 @@
+package com.mazeu.bookstore.repositories;
+
+import com.mazeu.bookstore.domain.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer> {
+
+}
