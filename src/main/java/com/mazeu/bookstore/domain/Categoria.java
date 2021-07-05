dafca5088d9495681854ba71164f5777ce199389
mazeu <marciomazeu@hotmail.com>
2021-07-05 20:28:04 -0300

    Classes almost finished

--- src/main/java/com/mazeu/bookstore/domain/Categoria.java
+++ src/main/java/com/mazeu/bookstore/domain/Categoria.java
@@ -1,2 +1,61 @@
-package com.mazeu.bookstore.domain;public class Categoria {
+package com.mazeu.bookstore.domain;
+
+import org.hibernate.annotations.GeneratorType;
+import org.yaml.snakeyaml.events.Event;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Categoria implements Serializable {
+
+    private static final Long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    private String descricao;
+
+    @OneToMany(mappedBy = "categoria")
+    private List<Livro> livros = new ArrayList<>();
+
+
+    public Categoria(){
+        super();
+    }
+
+    public Categoria(Integer id, String name, String descricao) {
+        this.id = id;
+        this.name = name;
+        this.descricao = descricao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
--- src/main/java/com/mazeu/bookstore/domain/Livro.java
+++ src/main/java/com/mazeu/bookstore/domain/Livro.java
@@ -1,2 +1,74 @@
-package com.mazeu.bookstore.domain;public class Livro {
+package com.mazeu.bookstore.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Livro implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+
+    private String titulo;
+
+    private String nameAutor;
+
+    private String texto;
+
+    @ManyToOne
+    @Column(name = "categoria_id")
+    private Categoria categoria;
+
+    public Livro(Integer id, String titulo, String nameAutor, String texto, Categoria categoria) {
+        this.id = id;
+        this.titulo = titulo;
+        this.nameAutor = nameAutor;
+        this.texto = texto;
+        this.categoria = categoria;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getNameAutor() {
+        return nameAutor;
+    }
+
+    public void setNameAutor(String nameAutor) {
+        this.nameAutor = nameAutor;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
 }
